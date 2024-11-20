877f10940f880815115132d7785229bf00d51092
Peikkohi <peik.kalliokoski@helsinki.fi>
2024-11-20 16:34:11 +0200

    lisää appiin tietokantatieto

--- src/app.py
+++ src/app.py
@@ -1,15 +1,12 @@
 from flask import render_template, request, redirect  # , jsonify, flash
 
-# from db_helper import reset_db
-# from repositories.todo_repository import get_todos, create_todo, set_done
-from config import app  # , test_env
+from config import app, db  # , test_env
 
-# from util import validate_todo
 from repositories.reference_repository import Transaction
 
 counter = 0
 
-transaction = Transaction(database=SQLAlchemy(app))
+transaction = Transaction(database=db)
 
 @app.route("/")
 def index():
@@ -32,30 +29,3 @@ def submit_data():
     return redirect("/")
 
 
-# @app.route("/new_todo")
-# def new():
-#    return render_template("new_todo.html")
-#
-# @app.route("/create_todo", methods=["POST"])
-# def todo_creation():
-#    content = request.form.get("content")
-#
-#    try:
-#        validate_todo(content)
-#        create_todo(content)
-#        return redirect("/")
-#    except Exception as error:
-#        flash(str(error))
-#        return  redirect("/new_todo")
-#
-# @app.route("/toggle_todo/<todo_id>", methods=["POST"])
-# def toggle_todo(todo_id):
-#    set_done(todo_id)
-#    return redirect("/")
-#
-# testausta varten oleva reitti
-# if test_env:
-#    @app.route("/reset_db")
-#    def reset_database():
-#        reset_db()
-#        return jsonify({ 'message': "db reset" })
