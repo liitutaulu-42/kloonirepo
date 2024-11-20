1e861ab932d7ad0d5d1985d25e13a0b6c7b669b3
Peikkohi <peik.kalliokoski@helsinki.fi>
2024-11-20 23:48:42 +0200

    lisää kokeita artikkelin lisäys koodille

---
+++ src/tests/transaction_test.py
@@ -0,0 +1,44 @@
+import unittest
+
+from repositories.reference_repository import Transaction
+
+class SessionStub():
+    def __init__(self):
+        self.last_insert = tuple()
+
+    def execute(self, _, values):
+        self.last_insert = values
+
+    def commit(self):
+        pass
+
+class DatabaseStub:
+    def __init__(self):
+        self.session = SessionStub()
+
+class TestTransaction(unittest.TestCase):
+    def setUp(self):
+        self.db = DatabaseStub()
+        self.transaction = Transaction(database=self.db)
+
+    def test_initialize_with_no_transactions(self):
+        self.assertEqual(self.db.session.last_insert, tuple())
+
+    def test_fail_author_syntax(self):
+        with self.assertRaises(AssertionError):
+            self.transaction.insert_article(
+                    key="Doe2023", 
+                    author="3",
+                    title="Title of the Article",
+                    journal="",
+                    year=2024,
+                    )
+
+    def test_succeeds_with_author_first_to_last_syntax(self):
+        self.transaction.insert_article(
+                key="Doe2023", 
+                author="Isaac Newton",
+                title="Title of the Article",
+                journal="",
+                year=2024,
+                )
