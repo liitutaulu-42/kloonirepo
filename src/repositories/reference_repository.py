e47a266d0d93290325759fa6bd6bc192fee25fbf
Peikkohi <peik.kalliokoski@helsinki.fi>
2024-11-21 08:41:45 +0200

    muuta seuraamaan pylinttiä

--- src/repositories/reference_repository.py
+++ src/repositories/reference_repository.py
@@ -4,16 +4,17 @@ from sqlalchemy import text
 
 class Transaction:
     def __init__(self, database):
-        self.db = database
+        self.database = database
 
-    def is_kirjoittaja_valid(self, kirjoittaja):
+    @staticmethod
+    def is_kirjoittaja_valid(kirjoittaja):
         name = "[A-Za-z]+"
-        firstnames = "%s( %s)*" % (name, name)
-        first_last = "%s %s" % (firstnames, name)
-        comma_separated = "%s(, %s)?, %s" % (name, name, firstnames)
-        both_formats = "(%s|%s)" % (first_last, comma_separated)
-        all_kirjoittajat = "^%s( AND %s)*$" % (both_formats, both_formats)
-        return search(all_kirjoittajat, kirjoittaja) != None
+        firstnames = f"{name}( {name})*"
+        first_last = f"{firstnames} {name}"
+        comma_separated = f"{name}(, {name})?, {firstnames}"
+        both_formats = f"({first_last}|{comma_separated})"
+        all_kirjoittajat = f"^{both_formats}( AND {both_formats})*$"
+        return search(all_kirjoittajat, kirjoittaja) is not None
 
     def insert_article(self, koodi, kirjoittaja, otsikko, julkaisu, vuosi):
         assert self.is_kirjoittaja_valid(kirjoittaja), "Inputted kirjoittaja is invalid"
@@ -28,5 +29,5 @@ class Transaction:
             "INSERT INTO artikkelit (koodi, kirjoittaja, otsikko, julkaisu, vuosi) "
             "VALUES (:koodi, :kirjoittaja, :otsikko, :julkaisu, :vuosi)"
         )
-        self.db.session.execute(sql, values)
-        self.db.session.commit()
+        self.database.session.execute(sql, values)
+        self.database.session.commit()
