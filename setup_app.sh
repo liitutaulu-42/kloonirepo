a9db8f7821cce3698848f8ec65863d1b68730284
Joonas Tapio <joonas.tapio@helsinki.fi>
2024-11-26 15:55:23 +0200

    setup toimii

--- setup_app.sh
+++ setup_app.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 echo "Setup app"
-
+cd "$(dirname "$0")" || return
 FILE=.env
-if [ ! -f "$FILE" ]; then
+if [ -f "$FILE" ]; then
+	echo "tiedosto olemassa"
+else
+	touch $FILE
 	echo "$FILE tiedosto puuttuu, luodaan"
-	echo -e "DATABASE_URL=postgresql://postgres:postgres@localhost:5432/postgres\n" >$FILE
-	echo -e "TEST_ENV=true\n" >$FILE
-	echo "SECRET_KEY='sekret'" >$FILE
+	echo -e "DATABASE_URL=postgresql://postgres:postgres@localhost:5432/postgres\n" >>$FILE
+	echo -e "TEST_ENV=true\n" >>$FILE
+	echo "SECRET_KEY='sekret'" >>$FILE
 fi
 
 poetry run python3 src/db_helper.py
@@ -19,9 +22,13 @@ status=$?
 echo "started flask server"
 
 BROWSER=firefox
+BROWSER1=google-chrome
 
 if [ -n "$BROWSER" ]; then
 	$BROWSER 'http://127.0.0.1:5001/'
+elif [ -n "$BROWSER1" ]; then
+	$BROWSER1 'http://127.0.0.1:5001/'
+
 fi
 
 kill $(lsof -t -i:5001)
