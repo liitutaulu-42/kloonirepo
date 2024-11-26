1892c69d5699369991e7e2f62d3a6741153043cd
Joonas Tapio <joonas.tapio@helsinki.fi>
2024-11-26 14:16:31 +0200

    tietokanta alustuu nyt helpommin

--- src/config.py
+++ src/config.py
@@ -1,18 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+from os import getenv
 
-# from dotenv import load_dotenv
-# from os import getenv
-#
-# load_dotenv()
-#
-# test_env = "true"
-# print(f"Test environment: {test_env}")
-#
+load_dotenv()
+
+test_env = getenv("TEST_ENV") == "true"
+print(f"Test environment: {test_env}")
+
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = (
-    "postgresql://postgres:postgres@localhost:5432/postgres"
-)
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-#app.config["SECRET_KEY"] = "0"
+app.secret_key = getenv("SECRET_KEY")
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 db = SQLAlchemy(app)
