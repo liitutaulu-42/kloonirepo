ee51a121f6bd54a77b89d2d4e0e6c36bd9ecf19f
Peikkohi <26819965+Peikkohi@users.noreply.github.com>
2024-11-26 14:17:13 +0200

    lisää regex selvennyksiä

--- src/repositories/reference_repository.py
+++ src/repositories/reference_repository.py
@@ -8,11 +8,30 @@ class Transaction:
 
     @staticmethod
     def is_kirjoittaja_valid(kirjoittaja):
+        # Kirjoittajakentällä on esitysmuodot:
+        # 1. {Etunimet Sukunimi}
+        # 2. {Sukunimi, Etunimet}
+        # 3. {Sukunimi, Liite, Etunimet}
+        # tai kun on monta kirjoittajaa: kirjoittaja AND kirjoittaja
+
+        # Koodi käyttää regexiä ja formaattimerkkijonoja lomitusten, joka
+        # vaikeuttaa koodin lukua, mutta parantaa sen ajattelua.
+
+        # nimi esitetään aakkosisena merkkijonona, koska en löytänyt toista
+        # ohjeistusta
         name = "[A-Za-z]+"
+        # tosiaan montaa etunimeä tuetaan ja se vaikutti olevan ainoastaan
+        # etunimet väleillä erotettuina
         firstnames = f"{name}( {name})*"
+        # tämä käsittelee 1. esitysmuodon
         first_last = f"{firstnames} {name}"
+        # tämä loput eli 2. ja 3. esitysmuodot.
+        # liite esiintyy välillä, niin se on helppo kuvata regexillä
         comma_separated = f"{name}(, {name})?, {firstnames}"
+        # sitten yhdistetään esiintymismuodot
         both_formats = f"({first_last}|{comma_separated})"
+        # lopuksi tarkistamme, että koko merkkijono tottelee
+        # yksittäisen tai monen kirjoittajan syotettä.
         all_kirjoittajat = f"^{both_formats}( AND {both_formats})*$"
         return search(all_kirjoittajat, kirjoittaja) is not None
 
