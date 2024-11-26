fc7489e23556a1b04bfe99f9ab1f3c8b15a9abd0
Joonas Tapio <joonas.tapio@helsinki.fi>
2024-11-26 14:38:33 +0200

    kaikki toimii setupissa niinkuin pitää

--- setup_app.sh
+++ setup_app.sh
@@ -3,7 +3,7 @@
 echo "Setup app"
 
 FILE=.env
-if [ ! -f "$FILE"]; then
+if [ ! -f "$FILE" ]; then
 	echo "$FILE tiedosto puuttuu, luodaan"
 	echo "DATABASE_URL=postgresql://postgres:postgres@localhost:5432/postgres\n" >$FILE
 	echo "TEST_ENV=true" >$FILE
@@ -14,10 +14,16 @@ poetry run python3 src/db_helper.py
 
 echo "DB setup done"
 
-poetry run python3 src/index.py &
-
+poetry run python3 src/index.py
+status=$?
 echo "started flask server"
 
-if [ -n $BROWSER ]; then
+BROWSER=firefox
+
+if [ -n "$BROWSER" ]; then
 	$BROWSER 'http://127.0.0.1:5001/'
 fi
+
+kill $(lsof -t -i:5001)
+
+exit $status
