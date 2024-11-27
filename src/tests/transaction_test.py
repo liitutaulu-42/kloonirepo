055f18afc85448afe7fdfc382681c6c632776a02
Peikkohi <26819965+Peikkohi@users.noreply.github.com>
2024-11-27 19:09:50 +0200

    korjaa transaction testit

--- src/tests/transaction_test.py
+++ src/tests/transaction_test.py
@@ -1,42 +1,31 @@
 import unittest
+from unittest.mock import Mock, ANY
 
 from repositories.reference_repository import Transaction
 
-class SessionStub():
-    def __init__(self):
-        self.last_insert = tuple()
-
-    def execute(self, _, values):
-        self.last_insert = values
-
-    def commit(self):
-        pass
-
-class DatabaseStub:
-    def __init__(self):
-        self.session = SessionStub()
-
 class TestTransaction(unittest.TestCase):
     def setUp(self):
-        self.database = DatabaseStub()
+        self.database = Mock()
         self.transaction = Transaction(database=self.database)
 
     def test_initialize_with_no_transactions(self):
-        self.assertEqual(self.database.session.last_insert, tuple())
+        self.database.session.commit.assert_not_called()
 
     def test_insert_article_values_are_same(self):
-        koodi = "Doe2023"
-        kirjoittaja = "Isaac Newton"
-        otsikko = "Title of the Article"
-        julkaisu = ""
-        vuosi = 2024
-        self.transaction.insert_article(koodi, kirjoittaja, otsikko, julkaisu, vuosi)
-        last_insert = self.database.session.last_insert
-        self.assertEqual(last_insert["koodi"], koodi)
-        self.assertEqual(last_insert["kirjoittaja"], kirjoittaja)
-        self.assertEqual(last_insert["otsikko"], otsikko)
-        self.assertEqual(last_insert["julkaisu"], julkaisu)
-        self.assertEqual(last_insert["vuosi"], vuosi)
+        self.transaction.insert_article(
+                koodi="Doe2023",
+                kirjoittaja="Isaac Newton",
+                otsikko="Title of the Article",
+                julkaisu="",
+                vuosi="2024",
+            )
+        self.database.session.execute.assert_called_once_with(ANY, {
+            "koodi": ANY,
+            "kirjoittaja": "Isaac Newton",
+            "otsikko": "Title of the Article",
+            "julkaisu": "",
+            "vuosi": "2024",
+            })
 
     def test_fail_author_syntax(self):
         with self.assertRaises(AssertionError):
@@ -45,14 +34,5 @@ class TestTransaction(unittest.TestCase):
                     kirjoittaja="3",
                     otsikko="Title of the Article",
                     julkaisu="",
-                    vuosi=2024,
+                    vuosi="2024",
                     )
-
-    def test_succeeds_with_author_first_to_last_syntax(self):
-        self.transaction.insert_article(
-                koodi="Doe2023",
-                kirjoittaja="Isaac Newton",
-                otsikko="Title of the Article",
-                julkaisu="",
-                vuosi=2024,
-                )
