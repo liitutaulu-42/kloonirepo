a4caf46093d2b856a0454d1871100d256f073747
Joonas Tapio <joonas.tapio@helsinki.fi>
2024-12-05 13:34:39 +0200

    testejä lisätty

--- src/tests/transaction_test.py
+++ src/tests/transaction_test.py
@@ -3,6 +3,7 @@ from unittest.mock import Mock, call
 
 from transaction import Transaction
 
+
 class TestTransaction(unittest.TestCase):
     def setUp(self):
         self.database_handle = Mock()
@@ -14,39 +15,43 @@ class TestTransaction(unittest.TestCase):
     def test_insert_article_values_are_same(self):
         self.database_handle.create_entry.return_value = 1
         self.transaction.insert_article(
-                author="Isaac Newton",
-                title="Title of the Article",
-                journal="A",
-                year="2024",
-            )
+            author="Isaac Newton",
+            title="Title of the Article",
+            journal="A",
+            year="2024",
+        )
 
-        self.database_handle.create_entry \
-                .assert_called_once_with("article", "Isaac-Title-2024")
-        self.database_handle.add_field.assert_has_calls([
-            call(1, "author", "Isaac Newton"),
-            call(1, "title", "Title of the Article"),
-            call(1, "journal", "A"),
-            call(1, "year", "2024"),
-        ], any_order=True)
+        self.database_handle.create_entry.assert_called_once_with(
+            "article", "Isaac-Title-2024"
+        )
+        self.database_handle.add_field.assert_has_calls(
+            [
+                call(1, "author", "Isaac Newton"),
+                call(1, "title", "Title of the Article"),
+                call(1, "journal", "A"),
+                call(1, "year", "2024"),
+            ],
+            any_order=True,
+        )
         self.database_handle.commit.assert_called_once()
 
     def test_fail_author_syntax(self):
         with self.assertRaises(AssertionError):
             self.transaction.insert_article(
-                    author="3",
-                    title="Title of the Article",
-                    journal="A",
-                    year="2024",
-                    )
+                author="3",
+                title="Title of the Article",
+                journal="A",
+                year="2024",
+            )
 
     def test_insert_article_values_same_as_bibtex(self):
         self.database_handle.get_references.return_value = ((0, "testi"),)
         self.database_handle.get_fields_of.return_value = {
-                "author": "Kirjoittaja Nimi",
-                "journal": "J",
-                "title": "Testi Otsikko",
-                "year": "2024",
-                }
+            "author": "Kirjoittaja Nimi",
+            "journal": "J",
+            "title": "Testi Otsikko",
+            "year": "2024",
+        }
 
         bibtex = self.transaction.get_bibtex()
 
@@ -57,3 +62,28 @@ class TestTransaction(unittest.TestCase):
         self.assertIn("year = 2024", bibtex)
         self.assertIn(",\n\t", bibtex)
         self.assertIn("\n}", bibtex)
+
+    def test_insert_book(self):
+        self.database_handle.create_entry.return_value = 1
+        self.transaction.insert_book(
+            author="Albert Einstein",
+            title="suhteellinen kirja",
+            year="2000",
+            publisher="Otava kustanns",
+            address="manskun varrel",
+        )
+
+        self.database_handle.create_entry.assert_called_once_with(
+            "book", "Albert-suhteellinen-2000"
+        )
+        self.database_handle.add_field.assert_has_calls(
+            [
+                call(1, "author", "Albert Einstein"),
+                call(1, "title", "suhteellinen kirja"),
+                call(1, "year", "2000"),
+                call(1, "publisher", "Otava kustanns"),
+                call(1, "address", "manskun varrel"),
+            ],
+            any_order=True,
+        )
+        self.database_handle.commit.assert_called_once()
